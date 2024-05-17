7414ea09aea611fc3eae75fbeefda97125872924
YuriLopess <costalopesyuri@gmail.com>
2024-05-17 08:11:44 -0300

    Adding transitions.

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,113 @@
-const about = document.querySelector('.about')
+window.sr = ScrollReveal({reset: true});
 
-const myObserver = new IntersectionObserver((entries) => {
-    console.log(entries)
-})
+// Index - Home
+sr.reveal('.about-img', {origin: "right",
+distance: "100px", 
+duration: 3000,
+reset: false});
 
-myObserver.observe(about)
+sr.reveal('.about-text h2', {origin: "left",
+distance: "100px",
+duration: 4000,
+reset: false});
+
+sr.reveal('.about-text p', {origin: "left",
+distance: "200px", 
+duration: 4500,
+reset: false});
+
+sr.reveal('.about .buttons', {origin: "bottom",
+distance: "100px", 
+duration: 5000,
+reset: false});
+
+// Index - Products
+sr.reveal('.products .heading', {origin: "left",
+distance: "200px", 
+duration: 3000,
+reset: false});
+
+sr.reveal('#item-1', {origin: "left",
+distance: "200px", 
+duration: 4000,
+reset: false});
+
+sr.reveal('#item-2', {origin: "left",
+distance: "200px", 
+duration: 5000,
+reset: false});
+
+sr.reveal('#item-3', {origin: "left",
+distance: "200px", 
+duration: 6000,
+reset: false});
+
+sr.reveal('#item-4', {origin: "left",
+distance: "200px", 
+duration: 7000,
+reset: false});
+
+sr.reveal('.heading-button', {origin: "bottom",
+distance: "100px", 
+duration: 7100,
+reset: false});
+
+// Index - Customers
+sr.reveal('.customers .heading', {origin: "top",
+distance: "100px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('#box-1', {origin: "left",
+distance: "200px", 
+duration: 3000,
+reset: false});
+
+sr.reveal('#box-2', {origin: "left",
+distance: "100px", 
+duration: 4500,
+reset: false});
+
+sr.reveal('#box-3', {origin: "left",
+distance: "100px", 
+duration: 5500,
+reset: false});
+
+//AboutPage - Story
+sr.reveal('#place', {origin: "right",
+distance: "100px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.title', {origin: "left",
+distance: "200px",
+duration: 3000,
+reset: false});
+
+sr.reveal('.content', {origin: "left",
+distance: "200px", 
+duration: 3500,
+reset: false});
+
+//AboutPage - Blend
+sr.reveal('#blend', {origin: "left",
+distance: "100px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('#second-title', {origin: "right",
+distance: "200px",
+duration: 3000,
+reset: false});
+
+sr.reveal('.sub-title', {origin: "right",
+distance: "200px", 
+duration: 3500,
+reset: false});
+
+sr.reveal('.paragraph', {origin: "right",
+distance: "200px", 
+duration: 3700,
+reset: false});
+
+//Product - Products
